6ec2c13732871b4fb91617a25da27ca2442edad1
Arnold Jose Velasquez Ortiz <ajvelasquez@pucp.pe>
2019-02-05 16:24:28 -0500

    v0.2
    Se añade:
    Base.html para plantilla de trabajo(modificar alejandro)
    y carpeta static para css y imagenes

--- telefonica_test/settings.py
+++ telefonica_test/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR= os.path.join(BASE_DIR,'templates')
+STATIC_DIR= os.path.join(BASE_DIR,'static')
+
 print(TEMPLATE_DIR)
 
 # Quick-start development settings - unsuitable for production
@@ -38,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap3',
     't_test',
 ]
 
@@ -121,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+
+]
